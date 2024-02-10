48b841981d3ceda4c4b066a20297f7f33e809031
AngelesT85 <brawl-ford123456@mail.ru>
2024-02-10 20:11:28 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+
+bot = Bot(token = "6983802838:AAHF9Lm4bWZIXJr77u4RpafcUCjUIkKEeOY")
+
+dispatcher = Dispatcher()
+
+
+@dispatcher.message(CommandStart())
+async def start_command(message: types.Message):
+    await message.answer("It was start")
+
+
+@dispatcher.message()
+async def echo_message(message: types.Message):
+    await message.answer(message.text)
+ 
+ 
+
+
+async def main():
+    await dispatcher.start_polling(bot)
+
+
+
+asyncio.run(main())
