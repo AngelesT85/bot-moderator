b8cb4c975ea58ff08f8ee9964379d4e8cd5708e8
AngelesT85 <brawl-ford123456@mail.ru>
2024-02-11 19:34:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,24 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+# from aiogram.filters import CommandStart
 from config import TOKEN
 
 bot = Bot(token = TOKEN)
 
 dispatcher = Dispatcher()
 
-@dispatcher.message(CommandStart())
-async def say_hello(message: types.Message):
-    await message.answer("Hello!")
+@dispatcher.edited_message()
+async def check_of_changes(message: types.Message):
+    await message.reply("User change the message!")
 
-@dispatcher.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-    await message.reply(message.text)
+# @dispatcher.message(CommandStart())
+# async def say_hello(message: types.Message):
+#     await message.answer("Hello!")
+
+# @dispatcher.message()
+# async def echo(message: types.Message):
+#     await message.answer(message.text)
+#     await message.reply(message.text)
 
 async def main():
     await dispatcher.start_polling(bot)
