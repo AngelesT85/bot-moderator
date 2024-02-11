bfb1ab9eb78659d76b8c08e69b8bb4746fbf368c
AngelesT85 <brawl-ford123456@mail.ru>
2024-02-11 20:12:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,28 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
-# from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart
 from config import TOKEN
+
+ALLOWED_UPDATES = ["message", "edited_message"]
 
 bot = Bot(token = TOKEN)
 
 dispatcher = Dispatcher()
 
-@dispatcher.edited_message()
-async def check_of_changes(message: types.Message):
-    await message.reply("User change the message!")
+# @dispatcher.edited_message()
+# async def check_of_changes(message: types.Message):
+#     await message.reply(message.from_user.id, "User change the message!")
 
-# @dispatcher.message(CommandStart())
-# async def say_hello(message: types.Message):
-#     await message.answer("Hello!")
+@dispatcher.message(CommandStart())
+async def say_hello(message: types.Message):
+    await message.answer("Hello!")
 
-# @dispatcher.message()
-# async def echo(message: types.Message):
-#     await message.answer(message.text)
-#     await message.reply(message.text)
+@dispatcher.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+    await message.reply(message.text)
 
 async def main():
-    await dispatcher.start_polling(bot)
+    await dispatcher.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 asyncio.run(main())
