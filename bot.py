e2ae4a29a71193e7ceb48f2da3ae515e8e0b40ac
AngelesT85 <brawl-ford123456@mail.ru>
2024-02-11 19:23:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from time import time
-time_start = time()
+from config import TOKEN
 
-bot = Bot(token = "6983802838:AAHF9Lm4bWZIXJr77u4RpafcUCjUIkKEeOY")
+bot = Bot(token = TOKEN)
 
 dispatcher = Dispatcher()
 
 @dispatcher.message(CommandStart())
-async def rise_my_eyelids(message: types.Message):
-    now_time = time()
-    if (now_time - time_start) % 604800 == 0:
-        await bot.send_message(message.from_user.id, "rise_my_eyelids!".upper())
+async def say_hello(message: types.Message):
+    await message.answer("Hello!")
+
+@dispatcher.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+    await message.reply(message.text)
 
 async def main():
     await dispatcher.start_polling(bot)
